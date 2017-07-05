5863f77164cc4e909624b136e875c9c0ad4d703e
Yeap <Yeap@DESKTOP-CPH3CO8>
2017-07-05 14:28:23 +0800

    start

---
+++ Project-Gunslayah/Project-Gunslayah/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Project_Gunslayah.Global" Language="C#" %>
---
+++ Project-Gunslayah/Project-Gunslayah/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_Gunslayah.Startup))]
+namespace Project_Gunslayah
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
