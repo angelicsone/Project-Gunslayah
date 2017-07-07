692a72114a0ac46b01e5b3ca17caeb301cffdc1c
Reynold <Reynold@WIN-9MLOB8LSJFA>
2017-07-07 13:08:23 +0800

    asfasasdaf

--- Project-Gunslayah/Project-Gunslayah/Gunslayah Forum2.aspx.designer.cs
+++ Project-Gunslayah/Project-Gunslayah/Gunslayah Forum2.aspx.designer.cs
@@ -20,5 +20,14 @@ namespace Project_Gunslayah {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Panel Panel2;
+        
+        /// <summary>
+        /// HyperLink1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
     }
 }
